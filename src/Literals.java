1f3a25a9d9834e99fc3cb5e842b2fb1b0756b68d
fabian <fabian.maechler@noser.com>
2015-04-23 11:13:06 +0200

    Group large Numbers with "_"

--- src/Literals.java
+++ src/Literals.java
@@ -1,10 +1,10 @@
 public class Literals {
 
 	public static void main(String[] args) {
-		int a = 14; // decimal: 14
+		int a = 14_000; // decimal: 14'000
 		int b = 015; // octal: 8 + 5 = 13
 		int c = 0x16; // hexadecimal: 16 + 6 = 22
-
+		
 		System.out.format("a:%d b:%d c:%d", a, b, c);
 	}
 
