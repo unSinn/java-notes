e08468ea9806fab2f82def5c3861b230197d7349
fabian <fabian.maechler@noser.com>
2015-04-23 12:29:55 +0200

    Get rid of warnings.

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,13 +1,12 @@
 public class Arrays {
 
+	public static int[] a;
+	public static int[] b;
+	public static int c[];
+
 	public static void main(String[] args) {
-		int[] a;
-		int[] b;
-		int c[];
-		
 		a = new int[3];
 		a[0] = 42;
-		
 		System.out.println(a[0]);
 	}
 
