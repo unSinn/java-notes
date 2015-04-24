7a6e7dba1e0c95daec2489de720970b698a84904
Fabian Mächler <fabian.maechler@gmail.com>
2015-04-24 13:03:05 +0200

    Variable Initialization on the heap.

---
+++ src/VarInitOnHeap.java
@@ -0,0 +1,24 @@
+public class VarInitOnHeap {
+
+	int a;
+	int b = 0;
+
+	public static void main(String... a) {
+		VarInitOnHeap vioh = new VarInitOnHeap();
+		vioh.m(42);
+	}
+
+	public void m(int c) {
+		int d;
+		int e = 0;
+
+		a++; // this is ok, a is on the heap and heap variables are initialized
+				// with the default value.
+		b++;
+		c++;
+		// d++; -> this would not compile, d is on the stack and has to be
+		// initialized manually
+		e++;
+		System.out.print(a); // 1
+	}
+}
