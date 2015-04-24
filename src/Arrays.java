4a1415d4700d8743d07006aa07fc4fe6bb367faa
Fabian Mächler <fabian.maechler@gmail.com>
2015-04-24 11:16:10 +0200

    Array declaration fun...

--- src/Arrays.java
+++ src/Arrays.java
@@ -3,6 +3,7 @@ public class Arrays {
 	public static int[] a;
 	public static int[] b;
 	public static int c[];
+	public static int[] []d[];
 
 	public static void main(String[] args) {
 		a = new int[3];
