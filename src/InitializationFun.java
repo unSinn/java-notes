d0e0c91e5be9427654290286c4acf9c024c385e4
Fabian Mächler <fabian.maechler@gmail.com>
2015-04-27 11:23:19 +0200

    Added a less (or more) evil init example.

--- src/InitializationFun.java
+++ src/InitializationFun.java
@@ -3,7 +3,13 @@ public class InitializationFun {
 	static int a, b = 10;
 
 	public static void main(String... args) {
-		System.out.format("i: %d, j: %d", a, b);
+		System.out.format("i: %d, j: %d\n", a, b); // i: 0, j: 10
+
+		// But you could do something like this:
+		String s1, s2;
+		s1 = s2 = "Blublubb";
+		System.out.format("%s %s", s1, s2);
+
 	}
 
 }
