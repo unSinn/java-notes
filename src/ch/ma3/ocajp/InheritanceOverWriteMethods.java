9493b6cd03722582f2ee6cf495f6bbde865933d0
Fabian Mächler <fabian.maechler@gmail.com>
2015-04-28 15:53:27 +0200

    Some new Inheritance fun.

---
+++ src/ch/ma3/ocajp/InheritanceOverWriteMethods.java
@@ -0,0 +1,12 @@
+package ch.ma3.ocajp;
+
+interface I1 {
+	int getValue();
+}
+
+public abstract class InheritanceOverWriteMethods implements I1 {
+	void getValue(int a) { // This works because we don't overwrite the original
+							// method (different signature).
+		System.out.println("blubb");
+	}
+}
