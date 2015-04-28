da39e1aa0ab71a1c91900f2ea17410223ea1f0fe
Fabian Mächler <fabian.maechler@gmail.com>
2015-04-28 15:33:20 +0200

    Added static wildcard import.

--- src/ch/ma3/ocajp/StaticImports.java
+++ src/ch/ma3/ocajp/StaticImports.java
@@ -1,11 +1,13 @@
 package ch.ma3.ocajp;
 
+import static java.lang.System.*;
 import static java.lang.System.out;
 
 public class StaticImports {
 
 	public static void main(String[] args) {
 		out.println("blubb");
+		gc();
 	}
 
 }
