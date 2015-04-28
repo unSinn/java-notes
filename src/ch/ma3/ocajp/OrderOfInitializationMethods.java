543afcaa49c5346416a8fff24db442477810709f
Fabian Mächler <fabian.maechler@gmail.com>
2015-04-28 10:25:55 +0200

    There is no preference in static init methods. Just their order from top to bottom.

--- src/ch/ma3/ocajp/OrderOfInitializationMethods.java
+++ src/ch/ma3/ocajp/OrderOfInitializationMethods.java
@@ -1,4 +1,5 @@
 package ch.ma3.ocajp;
+
 public class OrderOfInitializationMethods {
 
 	private static String s = m("1");
@@ -7,12 +8,18 @@ public class OrderOfInitializationMethods {
 		s = m("2");
 	}
 
+	private static String x = m("3");
+
 	{
-		s = m("3");
+		s = m("i1");
 	}
 
 	public OrderOfInitializationMethods() {
-		s = m("4");
+		s = m("c1");
+	}
+
+	{
+		s = m("i2");
 	}
 
 	public static void main(String[] args) {
