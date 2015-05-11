fb0bc963a5db5cd15539f95cf7e998249c845880
Fabian Mächler <fabian.maechler@gmail.com>
2015-05-11 13:36:06 +0200

    Another funny Array declaration.

--- src/ch/ma3/ocajp/Arrays.java
+++ src/ch/ma3/ocajp/Arrays.java
@@ -1,15 +1,20 @@
 package ch.ma3.ocajp;
+
 public class Arrays {
 
 	public static int[] a;
 	public static int[] b;
 	public static int c[];
-	public static int[] []d[];
+	public static int[][] d[];
 
 	public static void main(String[] args) {
 		a = new int[3];
 		a[0] = 42;
 		System.out.println(a[0]);
+	}
+
+	static int foo()[] {
+		return null; // ye, here we should return a int[]
 	}
 
 }
